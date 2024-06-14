9b188e639841741f0a6a0af76d3954d16ada4f3a
regina-bosibori <msomegina@gmail.com>
2024-06-14 22:05:03 +0300

    products section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { IntroComponent } from './intro/intro.component';
 import { TrustedByComponent } from './trusted-by/trusted-by.component';
+import { OurProductsComponent } from './our-products/our-products.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent,IntroComponent,TrustedByComponent],
+  imports: [RouterOutlet, HeaderComponent,IntroComponent,TrustedByComponent,OurProductsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/our-products/our-products.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-our-products',
+  standalone: true,
+  imports: [],
+  templateUrl: './our-products.component.html',
+  styleUrl: './our-products.component.css'
+})
+export class OurProductsComponent {
+
+}
