75dd19bedbe77416dad2bc672da7e5f68a4a67c3
regina-bosibori <msomegina@gmail.com>
2024-06-17 00:10:31 +0300

    created new component- our approach

---
+++ src/app/our-approach/our-approach.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-our-approach',
+  standalone: true,
+  imports: [],
+  templateUrl: './our-approach.component.html',
+  styleUrl: './our-approach.component.css'
+})
+export class OurApproachComponent {
+
+}
