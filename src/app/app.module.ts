4ca493ab1e9955853ecdf49316897e7346d2433f
regina-bosibori <msomegina@gmail.com>
2024-06-21 23:47:48 +0300

    added app module file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { RouterModule } from '@angular/router';
 @NgModule ({
     imports: [
       MatSlideToggleModule,
+      CommonModule,
+      RouterModule
     ]
   })
   class AppModule {}
