c5948e2bfdc491cc27854f2954f31af0db88f3e4
regina-bosibori <msomegina@gmail.com>
2024-06-22 23:37:45 +0300

    iabout page intro content

---
+++ src/app/about-intro/about-intro.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-intro',
+  standalone: true,
+  imports: [],
+  templateUrl: './about-intro.component.html',
+  styleUrl: './about-intro.component.css'
+})
+export class AboutIntroComponent {
+
+}
