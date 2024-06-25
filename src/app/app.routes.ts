05d5902da2604588a5a6d3d2e65015953f7a95e1
regina-bosibori <msomegina@gmail.com>
2024-06-25 17:30:13 +0300

    added route for contacts page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { OurApproachComponent } from './our-approach/our-approach.component';
 import { OurProductsComponent } from './our-products/our-products.component';
 import { HomeComponent } from './home/home.component';
 import{AboutComponent} from './about/about.component'
+import { ContactpageComponent } from './contactpage/contactpage.component';
 
 export const routes: Routes = [
     {
@@ -17,5 +18,9 @@ export const routes: Routes = [
     {
         path: 'about',
         component: AboutComponent
+    },
+    {
+        path:'contacts',
+        component: ContactpageComponent
     }
 ];
