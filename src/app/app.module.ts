9b4a4bf8b9e34ee7841ba2b410b8de86d311e667
regina-bosibori <msomegina@gmail.com>
2024-07-15 15:03:27 +0300

    imported forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,14 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { RouterModule } from '@angular/router';
+import { FormsModule} from '@angular/forms';
 @NgModule ({
     imports: [
       MatSlideToggleModule,
       CommonModule,
-      RouterModule
+      RouterModule,
+      FormsModule,
+      
     ]
   })
   class AppModule {}
