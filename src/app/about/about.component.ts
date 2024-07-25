c72c6b5dff399b03a3a51633af23a5bdcd03da34
regina-bosibori <msomegina@gmail.com>
2024-07-25 17:38:32 +0300

    included our story component

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
 import {AboutIntroComponent} from '../about-intro/about-intro.component'
+import { OurstoryComponent } from '../ourstory/ourstory.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [HeaderComponent,AboutIntroComponent,FooterComponent],
+  imports: [HeaderComponent,AboutIntroComponent,FooterComponent,OurstoryComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
