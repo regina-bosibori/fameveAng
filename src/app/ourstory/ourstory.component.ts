c4d7248eb7a798a97286b856b3c74580385c31ea
regina-bosibori <msomegina@gmail.com>
2024-07-25 17:39:45 +0300

    selector ourstory component

---
+++ src/app/ourstory/ourstory.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ourstory',
+  standalone: true,
+  imports: [],
+  templateUrl: './ourstory.component.html',
+  styleUrl: './ourstory.component.css'
+})
+export class OurstoryComponent {
+
+}
